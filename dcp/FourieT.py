fa12a4a9b173e21198c3a7c1ebdf9c8b611a69ca
David Bruhwiler <bruhwiler@radiasoft.net>
2015-01-06 15:25:43 -0700

    Revert "Importing files from RadTrack subversion repository into Github"
    This reverts commit 3e136c508d4edb727515768cf630bb015eefc14d.

--- dcp/FourieT.py
+++
@@ -1,9 +0,0 @@
-import scipy
-import scipy.fftpack
-import numpy as np
-
-def FourT(signal,t, Np):
-    FFT_C=scipy.fft(signal)*2/Np
-    print np.shape(FFT_C)
-    freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
-    return (FFT_C, freqs)
