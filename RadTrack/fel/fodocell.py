6f570f3bf4dd3af22845d7c25544a4045d4772d9
sdwebb <swebb@radiasoft.net>
2015-02-04 11:55:59 -0700

    Updates to FODO cell design to give thick quads.

--- RadTrack/fel/fodocell.py
+++ RadTrack/fel/fodocell.py
@@ -5,26 +5,60 @@ import numpy as np
 
 class fodocell:
     def __init__(self):
-        self.f = False
+        self.quad_K = False
         self.Lu = False
+        self.quad_l = False
         self.transfer_matrix = np.matrix(np.zeros((4,4)))
 
-    def make_fodo_cell(self, Lu, f):
+    def make_fodo_cell(self, Lu, l, K):
         self.Lu = Lu
-        self.f = f
-        LuOf = self.Lu/self.f
-        stability = 1.-0.5*LuOf**2
-        if abs(stability) > 1.:
-            print '!Warning -- FODO lattice may be unstable'
+        self.quad_K = K
+        self.quad_l = l
+        rootK = np.sqrt(K)
+        rootKl = rootK*l
+        quadF = np.matrix(np.zeros((4,4)))
+        quadF[0,0] = np.cos(rootKl)
+        quadF[0,1] = np.sin(rootKl)/rootK
+        quadF[1,0] = -rootK*np.sin(rootKl)
+        quadF[1,1] = np.cos(rootKl)
+        quadF[2,2] = np.cosh(rootKl)
+        quadF[2,3] = np.sinh(rootKl)/rootK
+        quadF[3,2] = rootK*np.sinh(rootKl)
+        quadF[3,3] = np.cosh(rootKl)
 
-        self.transfer_matrix[0,0] = 1.+LuOf
-        self.transfer_matrix[0,1] = self.Lu*(2.+LuOf)
-        self.transfer_matrix[1,0] = -LuOf/self.f
-        self.transfer_matrix[1,1] = 1.-LuOf-LuOf**2
-        self.transfer_matrix[2,2] = 1.-LuOf
-        self.transfer_matrix[2,3] = self.Lu*(2.-LuOf)
-        self.transfer_matrix[3,2] = -LuOf/self.f
-        self.transfer_matrix[3,3] = 1.+LuOf-LuOf**2
+        quadD = np.matrix(np.zeros((4,4)))
+        quadD[0,0] = np.cosh(rootKl)
+        quadD[0,1] = np.sinh(rootKl)/rootK
+        quadD[1,0] = rootK*np.sinh(rootKl)
+        quadD[1,1] = np.cosh(rootKl)
+        quadD[2,2] = np.cos(rootKl)
+        quadD[2,3] = np.sin(rootKl)/rootK
+        quadD[3,2] = -rootK*np.sin(rootKl)
+        quadD[3,3] = np.cos(rootKl)
+
+        drift = np.matrix(np.zeros((4,4)))
+        drift[0,0] = 1.
+        drift[0,1] = Lu
+        drift[1,0] = 0.
+        drift[1,1] = 1.
+        drift[2,2] = 1.
+        drift[2,3] = Lu
+        drift[3,2] = 0.
+        drift[3,3] = 1.
+
+        map = np.dot(quadD, drift)
+        map = np.dot(drift, map)
+        map = np.dot(quadF, map)
+
+        print 'FODO map =', map
+        stability = 0.5*(map[0,0]+map[1,1])
+        if abs(stability) > 1.:
+            print '!Warning -- FODO lattice may be unstable in the horizontal'
+        stability = 0.5*(map[2,2]+map[3,3])
+        if abs(stability) > 1.:
+            print '!Warning -- FODO lattice may be unstable in the vertical'
+
+        self.transfer_matrix = map
 
         print self.transfer_matrix
 
@@ -36,10 +70,10 @@ class fodocell:
         return phix, phiy
 
     def compute_average_beta(self):
-        if self.f and self.Lu:
+        if self.quad_K and self.Lu and self.quad_l:
             print self.transfer_matrix
             phix, phiy = self.compute_phase_advance()
-            beta_average = self.Lu/phix+self.Lu/phiy
+            beta_average = (self.Lu+self.quad_l)/phix+(self.Lu+self.quad_l)/phiy
             return beta_average
         else:
             print 'FODO cell not specified'
@@ -49,7 +83,7 @@ class fodocell:
         return self.transfer_matrix
 
     def get_twiss_parameters(self):
-        if self.f and self.Lu:
+        if self.quad_K and self.Lu and self.quad_l:
             phix, phiy = self.compute_phase_advance()
             sinphix = np.sin(phix)
             sinphiy = np.sin(phiy)
