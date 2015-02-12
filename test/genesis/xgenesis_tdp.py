706c6f68293e9390d9ca38976e27bdfd0acfdae1
sdwebb <swebb@radiasoft.net>
2015-02-12 16:03:34 -0700

    Added additional example to play with.

--- test/genesis/xgenesis_tdp.py
+++ test/genesis/xgenesis_tdp.py
@@ -3,13 +3,8 @@ __author__ = 'swebb'
 from RadTrack.genesis.rbXGenesisTDep import RbXGenesisTDep
 
 test_plotter = RbXGenesisTDep()
-
-print 'plotter generated'
-
 test_plotter.parse_output('xgenesis_testdata_tdep.out')
-
-print 'data parsed'
 
 test_plotter.plot_data('s', 'Power')
 
-print 'data plotted'
+test_plotter.plot_data('z', 'Far Field')
