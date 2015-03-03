f15ea19ac63e0d0d58ba6b13d9961486e1a3f8e5
sdwebb <swebb@radiasoft.net>
2015-03-03 10:09:44 -0700

    Tests referenced simulation results that are not in the repo. Have changed tests to only look at data in the repo.

--- test/genesis/xgenesis.py
+++ test/genesis/xgenesis.py
@@ -4,13 +4,11 @@ from RadTrack.genesis.rbXGenesisTInd import RbXGenesisTInd
 
 test_plotter = RbXGenesisTInd()
 
-#test_plotter.parse_output('lcls.out')
-test_plotter.parse_output('euvl.out')
-#test_plotter.parse_output('xgenesis_testdata.out')
+test_plotter.parse_output('xgenesis_testdata.out')
 
-#test_plotter.set_ploterrors()
+test_plotter.set_ploterrors()
 
-#test_plotter.set_semilog()
+test_plotter.set_semilog()
 
 power, length = test_plotter.compute_saturation()
 print 'Saturation Power =', power, 'Watts'
