precision variables fix
fix snafu when I replaced all the precision variables

RbSrwmulti.py
@@ -21,8 +21,8 @@ class rbsrw(QtGui.QWidget):
         self.up = UP()
         self.beam = SRWLPartBeam()
         #self.precis = Precis()
-        self.self.arPrecF = [0]*5
-        self.self.arPrecP = [0]*5 
+        self.arPrecF = [0]*5
+        self.arPrecP = [0]*5 
         self.thick(self.ui.deparg.currentIndex())
 
         #set srw initial values
@@ -181,32 +181,32 @@ class rbsrw(QtGui.QWidget):
         
     def GetPrecision(self,dialog):
         #for spectral flux vs photon energy
-        self.self.arPrecF[0] = float(dialog.ui.harma.text()) #initial UR harmonic to take into account
-        self.self.arPrecF[1] = float(dialog.ui.harmb.text()) #final UR harmonic to take into account
-        self.self.arPrecF[2] = float(dialog.ui.lip.text()) #longitudinal integration precision parameter
-        self.self.arPrecF[3] = float(dialog.ui.aip.text()) #azimuthal integration precision parameter
-        self.self.arPrecF[4] = dialog.ui.flux.currentIndex()+1 #calculate flux (1) or flux per unit surface (2)
+        self.arPrecF[0] = float(dialog.ui.harma.text()) #initial UR harmonic to take into account
+        self.arPrecF[1] = float(dialog.ui.harmb.text()) #final UR harmonic to take into account
+        self.arPrecF[2] = float(dialog.ui.lip.text()) #longitudinal integration precision parameter
+        self.arPrecF[3] = float(dialog.ui.aip.text()) #azimuthal integration precision parameter
+        self.arPrecF[4] = dialog.ui.flux.currentIndex()+1 #calculate flux (1) or flux per unit surface (2)
         
         #for power density
-        self.self.arPrecP[0] = float(dialog.ui.prefact.text()) #precision factor
-        self.self.arPrecP[1] = dialog.ui.field.currentIndex()+1 #power density computation method (1- "near field", 2- "far field")
-        self.self.arPrecP[2] = float(dialog.ui.ilp.text()) #initial longitudinal position (effective if self.arPrecP[2] < self.arPrecP[3])
-        self.self.arPrecP[3] = float(dialog.ui.flp.text()) #final longitudinal position (effective if self.arPrecP[2] < self.arPrecP[3])
-        self.self.arPrecP[4] = int(dialog.ui.np.text()) #number of points for (intermediate) trajectory calculation
+        self.arPrecP[0] = float(dialog.ui.prefact.text()) #precision factor
+        self.arPrecP[1] = dialog.ui.field.currentIndex()+1 #power density computation method (1- "near field", 2- "far field")
+        self.arPrecP[2] = float(dialog.ui.ilp.text()) #initial longitudinal position (effective if self.arPrecP[2] < self.arPrecP[3])
+        self.arPrecP[3] = float(dialog.ui.flp.text()) #final longitudinal position (effective if self.arPrecP[2] < self.arPrecP[3])
+        self.arPrecP[4] = int(dialog.ui.np.text()) #number of points for (intermediate) trajectory calculation
         #return (self.arPrecF, self.arPrecP)
         
     def ShowPrecision(self,dialog):
-        dialog.ui.harma.setText(str(self.self.arPrecF[0]))
-        dialog.ui.harmb.setText(str(self.self.arPrecF[1]))
-        dialog.ui.lip.setText(str(self.self.arPrecF[2]))
-        dialog.ui.aip.setText(str(self.self.arPrecF[3]))
-        dialog.ui.flux.setCurrentIndex(self.self.arPrecF[4])
+        dialog.ui.harma.setText(str(self.arPrecF[0]))
+        dialog.ui.harmb.setText(str(self.arPrecF[1]))
+        dialog.ui.lip.setText(str(self.arPrecF[2]))
+        dialog.ui.aip.setText(str(self.arPrecF[3]))
+        dialog.ui.flux.setCurrentIndex(self.arPrecF[4])
         
-        dialog.ui.prefact.setText(str(self.self.arPrecP[0]))
-        dialog.ui.field.setCurrentIndex(self.self.arPrecP[1]-1)
-        dialog.ui.ilp.setText(str(self.self.arPrecP[2]))
-        dialog.ui.flp.setText(str(self.self.arPrecP[3]))
-        dialog.ui.np.setText(str(self.self.arPrecP[4]))
+        dialog.ui.prefact.setText(str(self.arPrecP[0]))
+        dialog.ui.field.setCurrentIndex(self.arPrecP[1]-1)
+        dialog.ui.ilp.setText(str(self.arPrecP[2]))
+        dialog.ui.flp.setText(str(self.arPrecP[3]))
+        dialog.ui.np.setText(str(self.arPrecP[4]))
         
     def makeund(self):
         dialog = DialogU()