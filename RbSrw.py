5100892af77b2400a0e16100fee527bc02a108cd
robnagler <github@q33.us>
2015-03-07 15:32:08 -0700

    import 'Radtrack' mispelled

--- RbSrw.py
+++ RbSrw.py
@@ -5,15 +5,15 @@ Copyright (c) 2013 RadiaBeam Technologies. All rights reserved
 
 import sys
 from PyQt4 import QtGui, QtCore
-from Radtrack.gui.RbBunchInterface import *
+from RadTrack.gui.RbBunchInterface import *
 
 class RbSrw(QtGui.QWidget):
     def __init__(self, parent = None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_bunchInterface()
         self.ui.setupUi(self)
-        
-        
+
+
 def main():
     app = QtGui.QApplication(sys.argv)
     myapp = RbSrw()
@@ -22,4 +22,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
