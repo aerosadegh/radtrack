60bdb67c358e2a33f31b6f0f87c5cba02c1c1278
robnagler <github@q33.us>
2015-03-19 05:24:49 -0600

    need to protect b_install_pyenv even more

--- install/fedora/install-exec-user.sh
+++ install/fedora/install-exec-user.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-set -e
+#### set -e
 # /tmp may be small
 export TEMP=/var/tmp
 curl -s -L https://raw.githubusercontent.com/biviosoftware/home-env/master/install.sh | bash
+(
+    set +e
+    . ~/.bashrc
+    b_install_pyenv 2
+    exit 0
+)
+cat > ~/.post.bashrc << 'EOF'
+py2
+EOF
 . ~/.bashrc
-b_install_pyenv 2
 curl -s -L https://raw.githubusercontent.com/radiasoft/foss-mirror/master/install-as-user.sh | bash
 cd ~/src/biviosoftware
 git clone -q https://github.com/biviosoftware/pybivio
@@ -18,6 +26,3 @@ cd radtrack
 #   ImportError: No module named numpy.distutils.core
 pip install numpy
 pip install -e .
-cat > ~/.post.bashrc << 'EOF'
-py2
-EOF
