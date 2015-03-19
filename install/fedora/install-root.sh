a6ac84337bae9d08358bb1252e0f72779bf869d9
robnagler <github@q33.us>
2015-03-18 22:06:02 -0600

    add swap to vbox; do not add stty -echo

--- install/fedora/install-root.sh
+++ install/fedora/install-root.sh
@@ -2,14 +2,13 @@
 set -e
 export HOME=/root
 cp -a /etc/skel/.??* /root
-cat > /.bashrc << 'EOF'
+
+if [ -f /.dockerinit ]; then
+    cat > /.bashrc << 'EOF'
 export HOME=/root
 cd $HOME
 . /root/.bash_profile
 EOF
-
-cat > $HOME/.post.bashrc <<'EOF'
-tty --silent && stty -echo
-EOF
+fi
 
 curl –s -L https://raw.githubusercontent.com/biviosoftware/home-env/master/install.sh | bash
--- install/fedora/install.sh
+++ install/fedora/install.sh
@@ -4,6 +4,15 @@ umask 022
 cfg=/cfg
 chmod -R a+rX /cfg
 
+/sbin/lsmod | grep -i -s -q vbox
+if [ $? == 0 -a ! -f /swap ]; then
+    dd if=/dev/zero of=/swap bs=1M count=1024
+    mkswap /swap
+    chmod 600 /swap
+    swapon /swap
+    echo '/swap none swap sw 0 0' >> /etc/fstab
+fi
+
 yum --quiet --assumeyes install $(cat /cfg/yum-install.list)
 curl -s -L https://raw.githubusercontent.com/radiasoft/foss-mirror/master/install-as-root.sh | bash
 
