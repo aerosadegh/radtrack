6dbe2d543d61d7a65c25d3158bf027873ddf2653
Mark Harrison <harrison@radiabeam.com>
2015-03-25 17:48:10 -0700

    Simplified unit conversion code and tests.

--- examples/UnitTests/felCalcTest.py
+++ examples/UnitTests/felCalcTest.py
@@ -1,34 +1,30 @@
 print 'FEL Calculations Test ...'
-try:
-    from RbFEL import RbFEL
+from RbFEL import RbFEL
 
-    a = RbFEL()
-    a.ui.charge.setText('300 pC')
-    a.ui.slicemit.setText('1 mm*mrad')
-    a.ui.ebeamenergy.setText('600 MeV')
-    a.ui.energyspread.setText('0.01%')
-    a.ui.bunchlen.setText('1.5 ps')
-    a.ui.reprate.setText('150 kHz')
-    a.ui.ufield.setText('0.9 T')
-    a.ui.beta.setText('1.0 m')
-    a.ui.radiatedwavelength.setText('13.5 nm')
+a = RbFEL()
+a.ui.charge.setText('300 pC')
+a.ui.slicemit.setText('1 mm*mrad')
+a.ui.ebeamenergy.setText('600 MeV')
+a.ui.energyspread.setText('0.01%')
+a.ui.bunchlen.setText('1.5 ps')
+a.ui.reprate.setText('150 kHz')
+a.ui.ufield.setText('0.9 T')
+a.ui.beta.setText('1.0 m')
+a.ui.radiatedwavelength.setText('13.5 nm')
 
-    expectedAveragePower = '39.785 W'
-    expectedSaturationLength = '12.023 m'
-    expectedUndulatorPeriod = '1.7702 cm'
-    failed = False
-    if a.ui.averagepower.text() != expectedAveragePower:
-        print 'Average Power Calculated:', a.ui.averagepower.text(), 'Expected:', expectedAveragePower
-        failed = True
-    if a.ui.uperiod.text() != expectedUndulatorPeriod:
-        print 'Undulator Period Calculated:', a.ui.uperiod.text(), 'Expected:', expectedUndulatorPeriod
-        failed = True
-    if a.ui.saturation.text() != expectedSaturationLength:
-        print 'Saturation Length Calculated:', a.ui.saturation.text(), 'Expected:', expectedSaturationLength
-        failed = True
-    if failed:
-        raise Exception()
-except Exception as e:
-    print e
-    raise
+expectedAveragePower = '39.785 W'
+expectedSaturationLength = '12.023 m'
+expectedUndulatorPeriod = '1.7702 cm'
+failed = False
+if a.ui.averagepower.text() != expectedAveragePower:
+    print 'Average Power Calculated:', a.ui.averagepower.text(), 'Expected:', expectedAveragePower
+    failed = True
+if a.ui.uperiod.text() != expectedUndulatorPeriod:
+    print 'Undulator Period Calculated:', a.ui.uperiod.text(), 'Expected:', expectedUndulatorPeriod
+    failed = True
+if a.ui.saturation.text() != expectedSaturationLength:
+    print 'Saturation Length Calculated:', a.ui.saturation.text(), 'Expected:', expectedSaturationLength
+    failed = True
+if failed:
+    raise Exception()
 print 'Passed.'
