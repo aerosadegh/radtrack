040843de29bfdceea5798fa1b639d8a55f2004cc
robnagler <github@q33.us>
2015-03-26 13:32:06 +0900

    use lexists to catch a file that's a dead link; improved file testing

--- tests/genesis/lattice_file_test.py
+++ tests/genesis/lattice_file_test.py
@@ -14,13 +14,10 @@ import pytest
 import numpy as np
 from radtrack.genesis import lattice_file
 
-def test_conformance():
-    """Read the file and make sure it is formated correctly"""
-    fn = 'lattice_file_test.out'
-    if os.path.exists(fn):
-        os.remove(fn)
-    unit_length = 3.1415
-    elems = {
+DEFAULT_PARAMS = {
+    'filename': 'lattice_file_test.out',
+    'unit_length': 3.1415,
+    'elems': {
         'QF': [
             np.array([1.1, 1.2, 1.3]),
             np.array([2.1, 2.2, 2.3]),
@@ -30,8 +27,15 @@ def test_conformance():
             np.array([4.1, 4.2, 4.3]),
             np.array([5.1, 5.2, 5.3]),
         ],
-    }
-    lattice_file.write(fn, elems, unit_length)
+    },
+}
+
+def test_conformance1():
+    """Read the file and make sure it is formated correctly"""
+    fn = DEFAULT_PARAMS['filename']
+    if os.path.exists(fn):
+        os.remove(fn)
+    lattice_file.write(**DEFAULT_PARAMS)
     with open(fn) as f:
         res = f.read()
     def a(p, flags=re.MULTILINE):
@@ -39,3 +43,17 @@ def test_conformance():
     a(r'--\nQF    1\.1    1\.2    1\.3    \n')
     a(r'  \nAW    5\.1    5\.2    5\.3    \n$', flags=0)
     a(r'QF +1.1 .*\nQF +2.1 .*\n(?:#.*\n)+AW +3.1 .*\nAW +4.1 .*\nAW +5.1 ')
+
+def test_conformance2():
+    """Make sure the file is removed first"""
+    fn = DEFAULT_PARAMS['filename']
+    if os.path.exists(fn):
+        os.remove(fn)
+    tag = 'some magic string to find'
+    with open(fn, 'w') as f:
+        f.write(tag)
+    lattice_file.write(**DEFAULT_PARAMS)
+    with open(fn) as f:
+        res = f.read()
+    assert not re.search(tag, res), 'file was not removed'
+    assert re.search('VERSION', res)
