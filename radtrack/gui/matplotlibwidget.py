43f65e4bd1be076245e3c0cb20780d434f527a08
Mark Harrison <harrison@radiabeam.com>
2015-03-25 18:43:48 -0700

    Replaced changed library with original.

--- radtrack/gui/matplotlibwidget.py
+++ radtrack/gui/matplotlibwidget.py
@@ -2,7 +2,7 @@
 from PyQt4 import QtGui
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas 
-from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as Navigationtoolbar
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as Navigationtoolbar
 from matplotlib.figure import Figure
 
 
