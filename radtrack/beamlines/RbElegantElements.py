a14e9b8d577b351d53d1e9edd1cdb7076a1d6760
Mark Harrison <harrison@radiabeam.com>
2015-03-26 19:49:03 -0700

    Non-numeric parameters need quotes in elegant files

--- radtrack/beamlines/RbElegantElements.py
+++ radtrack/beamlines/RbElegantElements.py
@@ -303,11 +303,18 @@ class elegantElement(elementCommon):
             quote = ''
         declaration = quote + self.name + quote + ':  ' + type(self).__name__ + ', '
 
-        sentence = [(param, parseUnits(datum)) for param, datum in \
+        phrases = [[param, parseUnits(datum)] for param, datum in \
                 zip(self.parameterNames, self.data) if datum != '']
 
+        # Non-numeric values need quoting
+        for phrase in phrases:
+            try:
+                float(phrase[1])
+            except ValueError:
+                phrase[1] = '"' + phrase[1] + '"'
+
         #add "=" in within phrases, then add ', ' between those groups
-        sentence = ', '.join(['='.join(phrase) for phrase in sentence])
+        sentence = ', '.join(['='.join(phrase) for phrase in phrases])
 
         line = declaration + sentence
         return wordwrap(line, 80, ' &', len(declaration))
