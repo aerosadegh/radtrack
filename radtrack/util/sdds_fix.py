de1ba88034037acbc271a70ea1eddee515c6c15d
robnagler <github@q33.us>
2015-04-07 22:16:59 +0900

    sdds module removed from radtrack repo so import sdds directly;
    don't use sddsdatamodule. That's a quirk of python's module search path.

--- radtrack/util/sdds_fix.py
+++ radtrack/util/sdds_fix.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Fix sdds imports
+"""Deprecated. Correct usage::
+
+    import sdds
+
+Do not use sddsdata directly.
 
 :copyright: Copyright (c) 2015 RadiaBeam Technologies, LLC.  All Rights Reserved.
 :license: Apache, see LICENSE for more details.
 """
-try:
-    import sdds
-#    import sddsdatamodule as sddsdata
-    import sddsdata
-except ImportError:
-    import radtrack.dcp.sdds as sdds
-    import radtrack.dcp.sddsdata as sddsdata
+import sdds
+from sdds import sddsdata
