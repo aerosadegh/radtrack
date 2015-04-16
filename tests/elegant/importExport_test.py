3af11e75e15e6c9e9ac2fca9bafc511649a39be9
Mark Harrison <harrison@radiabeam.com>
2015-04-15 19:22:33 -0700

    Test exported .lte files by running elegant on them
    New test for importExport_test.py. Three files had to be excluded (by
    renaming) because they referenced unavailable files for some of their
    elements.

--- tests/elegant/importExport_test.py
+++ tests/elegant/importExport_test.py
@@ -1,4 +1,5 @@
-import os, glob, sys
+import os, glob, sys, subprocess
+from itertools import izip_longest
 import radtrack.beamlines.RbElegantElements as ele
 import radtrack.beamlines.RbOpticalElements as opt
 from radtrack.RbUtility import insideQuote
@@ -16,8 +17,9 @@ app = QtGui.QApplication(sys.argv)
 
 exportEnd = '_export.lte'
 
-particleFileList = glob.glob(
-        os.path.join(os.getcwd(), 'deprecated', 'elegant', 'beamlines', '*.lte'))
+elegantFilesLocation = os.path.join(os.getcwd(), 'deprecated', 'elegant')
+elegantTestFile = os.path.join(elegantFilesLocation, 'elegantTest.ele')
+particleFileList = glob.glob(os.path.join(elegantFilesLocation, 'beamlines', '*.lte'))
 
 opticalFileList = glob.glob(
         os.path.join(os.getcwd(), 'deprecated', 'laser_transport', '*.rad'))
@@ -25,6 +27,57 @@ opticalFileList = glob.glob(
 fileLists = [particleFileList, opticalFileList]
 fileHandlers = [ele, opt]
 
+elegantSimTemplate = '''&run_setup
+    lattice = "%s",
+    use_beamline = %s,
+    default_order = 2,
+    p_central_mev = 200.0,
+    output = %%s.out,
+    centroid = %%s.cen,
+    sigma = %%s.sig,
+    final = %%s.fin,
+    parameters = %%s.param,
+    magnets = %%s.mag,
+    random_number_seed = 987654321,
+    combine_bunch_statistics = 0,
+    concat_order = 2,
+    tracking_updates = 1,
+    echo_lattice = 0,
+&end
+
+&run_control
+    n_steps = 1,
+    reset_rf_for_each_step = 1,
+&end
+
+&twiss_output
+    matched = 0,
+    concat_order = 3,
+    beta_x = 5, alpha_x = 0,
+    beta_y = 5, alpha_y = 0,
+    output_at_each_step = 1,
+    statistics = 1,
+    concat_order = 3,
+    filename = %%s.twi,
+&end
+
+&sdds_beam
+    input = "%s",
+&end
+
+&matrix_output
+    SDDS_output = %%s.mat,
+    SDDS_output_order = 3,
+    output_at_each_step = 1,
+&end
+
+&track
+&end
+
+&stop
+&end'''
+
+sddsFileName = os.path.join(elegantFilesLocation, 'bunches', 'elegantSimTest.sdds')
 # Test that importing an .lte file and an exported version of that file
 # result in the same elements being created
 def test_import_export():
@@ -41,6 +94,24 @@ def test_import_export():
             elementDictionary2, _ = fileHandler.fileImporter(exportFileName)
 
             assert all([element1 == element2 for (element1, element2) in \
-                    zip(elementDictionary1.values(), elementDictionary2.values())])
+                    izip_longest(elementDictionary1.values(), elementDictionary2.values())])
 
-            os.remove(exportFileName)
+            # Test elegant simulation on exported file
+            try:
+                longestLength = 0
+                longest = None
+                for beamline in [el for el in elementDictionary2.values() if el.isBeamline()]:
+                    if len(beamline.data) > longestLength:
+                        longest = beamline
+                        longestLength = len(beamline.data)
+                if fileList == particleFileList:
+                    with open(elegantTestFile, 'w') as f:
+                        f.write(elegantSimTemplate % (exportFileName, longest.name, sddsFileName))
+                    assert subprocess.call(['elegant', elegantTestFile]) == 0
+            finally:
+                for fileName in glob.glob(os.path.splitext(elegantTestFile)[0] + '.*'):
+                    os.remove(fileName)
+                os.remove(exportFileName)
+
+if __name__ == '__main__':
+    test_import_export()
