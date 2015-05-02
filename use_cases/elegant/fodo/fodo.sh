da42b3b90467083100c666681752c0dd134f5eff
David Bruhwiler <bruhwiler@radiasoft.net>
2015-05-02 10:01:52 -0600

    modifying the FODO use case
    shortened names
    updated README and scripts

--- use_cases/elegant/fodo/fodo.sh
+++ use_cases/elegant/fodo/fodo.sh
@@ -2,12 +2,7 @@
 
 elegant *.ele
 
-
 sddsplot -unsup=y -col=s,betax -graphic=line,type=0 *.twi -col=s,betay -graphic=line,type=1 *.twi -col=s,Profile -overlay=xmode=norm,yfact=0.1 -graphic=line,type=2 *.mag   &
-
-#sddsplot *.bun -col=x,y -graphic=dot&
-#sddsplot *.out -col=x,y -graphic=dot&
-
 
 sddshist2d *.bun xybun.hist -col=x,y -xpar=81 -ypar=81 -smooth
 sddscontour xybun.hist -shade -inter=8,8
