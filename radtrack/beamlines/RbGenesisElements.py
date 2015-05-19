5efbe6e04d886b5d8586b21c1ed3cbcad63364aa
Mark Harrison <harrison@radiabeam.com>
2015-05-18 20:40:38 -0700

    Revert "Delete unnecessary code"
    This reverts commit 2bb9c46207fc4bc67b7ca29388663a3b9ee8797f.

--- radtrack/beamlines/RbGenesisElements.py
+++ radtrack/beamlines/RbGenesisElements.py
@@ -17,6 +17,7 @@ class genesisElement(elementCommon):
                 zip(self.parameterNames, self.data, self.units) if datum]
 
         sentence = '   '.join(self.data)
+        #sentence = ', '.join(['='.join(phrase) for phrase in sentence])
 
         return type(self).__name__ + '     ' + sentence
 
