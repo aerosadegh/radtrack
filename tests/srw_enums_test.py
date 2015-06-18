1300fccb8049051a9fcd539a448e8af970af2ec1
robnagler <github@q33.us>
2015-06-18 22:01:46 +0000

    enum values changed

--- tests/srw_enums_test.py
+++ tests/srw_enums_test.py
@@ -15,11 +15,11 @@ from radtrack import srw_enums
 def test_density_method():
     """Verify a couple of values exist"""
     d = srw_enums.DensityMethod
-    assert d(0) == d.NEAR_FIELD, \
-        'DensityMethod.NEAR_FIELD should be 0'
-    assert d(1).has_name('FAR_FIELD'), \
+    assert d(1) == d.NEAR_FIELD, \
+        'DensityMethod.NEAR_FIELD should be 1'
+    assert d(2).has_name('FAR_FIELD'), \
         'DensityMethod.FAR_FIELD has name FAR_FIELD'
-    assert d.from_anything('NEAR_FIELD').value == 0, \
+    assert d.from_anything('NEAR_FIELD').value == 1, \
         'from_anything(NEAR_FIELD) should return an enum with value 0'
-    assert d.from_anything(1) == d.FAR_FIELD, \
+    assert d.from_anything(2) == d.FAR_FIELD, \
         'from_anything(1) should return FAR_FIELD'
