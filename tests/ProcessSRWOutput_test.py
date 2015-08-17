ed4a879c7f7c9a4d53e53a1f69130475a68d057b
shaftan <tshaftan@yahoo.com>
2015-08-17 02:53:32 +0000

    Added function of calculating lenght of ID trajectory. Optimized programming throughtout the code, replace print statments, changed graphics. Now need to save calculated data to yaml format to compare  tests with Analytic formulas

--- tests/ProcessSRWOutput_test.py
+++ tests/ProcessSRWOutput_test.py
@@ -33,32 +33,39 @@ def maxelements(seq):
 
     return max_indices
 
-def FindingSpectralMaxima(seq, deltI):
-	print('size:',np.shape(seq)[0])
+def FindingArrayMaxima(seq, deltI):
+	pkdc('size:',np.shape(seq)[0])
 	if seq:
 		j=0
 		maxV=[]
 		maxI=[]
 		maxIj=0
 		for i in xrange(0, np.shape(seq)[0]-deltI-1):
-#			print(i, seq[i])
+			pkdc(i, seq[i])
 			if seq[i+1]>seq[i]:
 				maxVj=seq[i+1]
 				maxIj=i+1
-#				print(maxIj)
+				pkdc(maxIj)
 			if (maxIj>0) & (i>maxIj+deltI):
 				j=j+1
 				maxV.append(maxVj)
-				maxI.append(maxIj)
-#				print(j,maxI,maxV)
+				maxI.append(maxIj)				
 				maxIj=1000000
 	else:
 		print('Input array is empty')
+	pkdc('Maximum # and Intensity of UR harmoncis: ',maxI,maxV)
 	return (maxV, maxI)
+
+def Path_Length(x,y):
+	Path_Len=0
+	for i in xrange(0, np.shape(x)[0]-1):
+		Path_Len=Path_Len+ np.sqrt((y[i+1]-y[i])**2*1E-6 + (x[i+1]-x[i])**2) 
+	return (Path_Len) 
 
 def test_1():
 	d = pkunit.data_dir()
 	## Testing actual SRW calculations 
+	
 	##Reading SRW data SPECTRUM
 	IFileName="Spectrum.txt"
 	f=open(str(d.join(IFileName)),"r")#,1000)
@@ -70,12 +77,11 @@ def test_1():
 	    I_rad.append(words[1])	
 	I_radf=map(float,I_rad)
 	maxI=max(I_radf)
-#	print (I_radf)
+	pkdc(I_radf)
 	print('Spectral Amplitude, ph/s/mrad2',maxI)
-	print(I_radf.index(max(I_radf)))
+	pkdc(I_radf.index(max(I_radf)))
 	maxIn=maxelements(I_radf)
-	print(maxIn)
-	(maxV, maxI)=FindingSpectralMaxima(I_radf,5)
+	(maxV, maxI)=FindingArrayMaxima(I_radf,5)
 	print(maxI, maxV)
 	f.close()
 
@@ -83,20 +89,24 @@ def test_1():
 	IFileName="Trajectory.txt"
 	f=open(str(d.join(IFileName)),"r")#,10000)
 	z_dist=[]
-	x_trajectory=[]
+	x_traj=[]
 	for line in f.readlines():
 	    words = line.split()
 	    z_dist.append(words[0])
-	    x_trajectory.append(words[1])
-	minX=min(map(float, x_trajectory))
-	maxX=max(map(float, x_trajectory))
-#	print (np.shape(z_dist))
-	print ('Length of ID, m', max((map(float, z_dist))))
+	    x_traj.append(words[1])
+	x_trajectory=map(float, x_traj)
+	z_distance=map(float, z_dist)
+	minX=min(x_trajectory)
+	maxX=max(x_trajectory)
+	minZ=min(z_distance)
+	maxZ=max(z_distance)
+	print ('Length of ID, m', maxZ-minZ)
 	print('Oscillation Amplitude, mm',(maxX-minX)/2)
-#	print (x_trajectory[1:10])
+	L_trajectory=Path_Length(z_distance, x_trajectory)
+	print('Length of Trajectory, m', L_trajectory)
 	f.close()
 
-	#Plotting
+	##Plotting
 	plot(e_p,I_rad)
 	j=0
 	for i in maxI:
@@ -108,9 +118,9 @@ def test_1():
 	
 	grid()
 	plt.show()
-	plot(z_dist,x_trajectory)
-	(maxVt, maxIt)=FindingSpectralMaxima(map(float,x_trajectory),20)
-	print(maxIt, maxVt)
+	plot(z_dist,x_trajectory,'.b',linestyle="-")
+	(maxVt, maxIt)=FindingArrayMaxima(map(float,x_trajectory),20)
+	pkdc(maxIt, maxVt)
 	j=0
 	for i in maxIt:
 		plt.scatter(z_dist[i], maxVt[j], color='red')
