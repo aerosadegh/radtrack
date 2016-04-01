da28446c5ff36b0248bb57c8257ca0033b334b94
qedseung <seung@radiabeam.com>
2016-04-01 16:02:15 -0700

    warning message
    catch and warn about number of particles needing to be a factor of 4
    times the number of bins, previously accepts any factor of 4.

--- radtrack/genesis_controller.py
+++ radtrack/genesis_controller.py
@@ -168,12 +168,12 @@ class Base(rt_controller.Controller):
         )
         if pu.exec_():
             self.params[which] = pu.get_params()
-            if which is 'undulator' and self.params[which]['vertical_focus']+self.params[which]['horizontal_focus'] !=1.0:
+            if which is 'undulator' and self.params[which]['vertical_focus']+self.params[which]['horizontal_focus']!=1.0:
                 box = QtGui.QMessageBox()
                 box.setIcon(QtGui.QMessageBox.Warning)
                 box.setText('Sum of undulator horizontal and vertical focus should equal 1.')
                 box.exec_()
-            elif which is 'beam' and self.params[which]['num_particle']%4*self.params['particle_loading']['num_bins']  != 0:
+            elif which is 'beam' and self.params[which]['num_particle']%(4*self.params['particle_loading']['num_bins'])!= 0:
                 box = QtGui.QMessageBox()
                 box.setIcon(QtGui.QMessageBox.Warning)
                 box.setText('Number of Particles must be a multiple of 4*Number of Bins for Phase. \n (specified in particle loading)')
