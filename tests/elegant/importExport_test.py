b56f51ceb3a898fe1e2f36f573e47e08e238f4ee
Mark Harrison <harrison@radiabeam.com>
2016-06-17 23:07:11 +0000

    Fix Elegant import/export test file lists
    Also, there's no point in testing the Elegant executable since we can't
    fix it.

--- tests/elegant/importExport_test.py
+++ tests/elegant/importExport_test.py
@@ -9,121 +9,58 @@ if not os.getenv('RPN_DEFNS', None):
 
 exportEnd = '_export.lte'
 
-elegantFilesLocation = os.path.join(os.getcwd(), 'deprecated', 'elegant')
-elegantTestFile = os.path.join(elegantFilesLocation, 'elegantTest.ele')
-particleFileList = glob.glob(os.path.join(elegantFilesLocation, 'beamlines', '*.lte'))
-excludedList = ['dtSweep.lte',
-                'fourDipoleCSR.lte',
-                'lattice.lte',
-                'multiple.lte',
-                'mv15-c5-v2-ring.lte']
+elegantFilesLocation = os.path.join(os.getcwd(), 'use_cases', 'elegant')
+opticalFilesLocation = os.path.join(os.getcwd(), 'use_cases', 'laser_transport')
+particleSuffix = '.lte'
+opticalSuffix = '.rad'
 
-opticalFileList = glob.glob(os.path.join(os.getcwd(), 'deprecated', 'laser_transport', '*.rad'))
-
-fileLists = [particleFileList, opticalFileList]
+suffixes = [particleSuffix, opticalSuffix]
+fileLocations = [elegantFilesLocation, opticalFilesLocation]
 fileHandlers = [ele, opt]
 
-elegantSimTemplate = '''&run_setup
-    lattice = "%s",
-    use_beamline = %s,
-    default_order = 2,
-    p_central_mev = 200.0,
-    output = %%s.out,
-    centroid = %%s.cen,
-    sigma = %%s.sig,
-    final = %%s.fin,
-    parameters = %%s.param,
-    magnets = %%s.mag,
-    random_number_seed = 987654321,
-    combine_bunch_statistics = 0,
-    concat_order = 2,
-    tracking_updates = 1,
-    echo_lattice = 0,
-&end
+# Can't handle these files with RPN calcs outside element descriptions or other errors
+skipFiles = [os.path.join(os.getcwd(), n) for n in ['use_cases/elegant/elegantExamples/rfDeflectingCavity/lattice.lte',
+                                                    'use_cases/elegant/elegantExamples/ellipseComparison/par10h.lte',
+                                                    'use_cases/elegant/elegantExamples/chromaticResponse/full457MeV.lte',
+                                                    'use_cases/elegant/elegantExamples/beamBreakup/lattice.lte',
+                                                    'use_cases/elegant/elegantExamples/multibunchCollectiveEffects/APS-24Bunch-CBI/lattice.lte',
+                                                    'use_cases/elegant/elegantExamples/rampTunesWithBeam/par10h.lte',
+                                                    'use_cases/elegant/elegantExamples/pepperPot/lattice.lte']]
 
-&run_control
-    n_steps = 1,
-    reset_rf_for_each_step = 1,
-&end
-
-&twiss_output
-    matched = 0,
-    concat_order = 3,
-    beta_x = 5, alpha_x = 0,
-    beta_y = 5, alpha_y = 0,
-    output_at_each_step = 1,
-    statistics = 1,
-    concat_order = 3,
-    filename = %%s.twi,
-&end
-
-&sdds_beam
-    input = "%s",
-&end
-
-&matrix_output
-    SDDS_output = %%s.mat,
-    SDDS_output_order = 3,
-    output_at_each_step = 1,
-&end
-
-&track
-&end
-
-&stop
-&end'''
-sddsFileName = os.path.join(elegantFilesLocation, 'bunches', 'elegantSimTest.sdds')
 # Test that importing an .lte file and an exported version of that file
 # result in the same elements being created
 def test_import_export():
-    for fileList, fileHandler in zip(fileLists, fileHandlers):
-        for fileName in fileList:
-            elementDictionary1, default1 = fileHandler.fileImporter(fileName)
-            exportFileName = os.path.splitext(fileName)[0] + exportEnd
-            fileHandler.exportToFile(exportFileName, elementDictionary1, default1)
-            with open(exportFileName) as f:
-                # Check that lines are not split inside quotes
-                for line in f:
-                    assert not insideQuote(line, len(line) - 1)
-
-            elementDictionary2, default2 = fileHandler.fileImporter(exportFileName)
-
-            if not elementDictionary1 == elementDictionary2:
-                for name in elementDictionary1:
-                    e1 = elementDictionary1[name]
-                    e2 = elementDictionary2[name]
-                    if not e1 == e2:
-                        print fileName
-                        print name
-                        if not e1.isBeamline():
-                            assert compareNormalizedElementData(e1, e2)
-                        else:
-                            for index in range(max(len(e1.data), len(e2.data))):
-                                assert compareNormalizedElementData(e1.data[index], e2.data[index])
-            assert default1 == default2
-
-            # Test elegant simulation on exported file
-            # Choose the beam line with the most elements for testing
-            try:
-                if os.path.basename(fileName) in excludedList:
+    for suffix, fileLocation, fileHandler in zip(suffixes, fileLocations, fileHandlers):
+        for walkTuple in os.walk(fileLocation):
+            dirName = walkTuple[0]
+            for fileName in [os.path.join(dirName, name) for name in walkTuple[2] if name.endswith(suffix)]:
+                if fileName in skipFiles:
                     continue
+                elementDictionary1, default1 = fileHandler.fileImporter(fileName)
+                exportFileName = os.path.splitext(fileName)[0] + exportEnd
+                fileHandler.exportToFile(exportFileName, elementDictionary1, default1)
+                with open(exportFileName) as f:
+                    # Check that lines are not split inside quotes
+                    for line in f:
+                        assert not insideQuote(line, len(line) - 1)
 
-                longestLength = 0
-                longest = None
-                for beamline in [el for el in elementDictionary2.values() if el.isBeamline()]:
-                    length = beamline.getNumberOfElements()
-                    if length > longestLength:
-                        longest = beamline
-                        longestLength = length
-                if fileList == particleFileList:
-                    with open(elegantTestFile, 'w') as f:
-                        f.write(elegantSimTemplate % (exportFileName, longest.name, sddsFileName))
-                    assert subprocess.call(['elegant', elegantTestFile]) == 0
-            finally:
-                for fileName in glob.glob(os.path.splitext(elegantTestFile)[0] + '.*') \
-                        + glob.glob('*.out') \
-                        + [exportFileName]:
-                    os.remove(fileName)
+                elementDictionary2, default2 = fileHandler.fileImporter(exportFileName)
+
+                if not elementDictionary1 == elementDictionary2:
+                    for name in elementDictionary1:
+                        e1 = elementDictionary1[name]
+                        e2 = elementDictionary2[name]
+                        if not e1 == e2:
+                            print fileName
+                            print name
+                            if not e1.isBeamline():
+                                assert compareNormalizedElementData(e1, e2)
+                            else:
+                                for index in range(max(len(e1.data), len(e2.data))):
+                                    assert compareNormalizedElementData(e1.data[index], e2.data[index])
+                assert default1 == default2
+                os.remove(exportFileName)
+
 
 def compareNormalizedElementData(e1, e2):
     print e1.data
